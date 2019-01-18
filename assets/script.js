f133ca138df8e2ae850520ba19b24b6808163122
AxonDragonScale <r.r.n.harkhani@gmail.com>
2019-01-18 14:50:27 +0530

    Fixed bug, collaborative editing now working.

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,7 @@ var initializeMarkdownEditor = function() {
     textArea.addEventListener('input', convertTextAreaToMarkdown);
 
     // pathname won't be greater than 1 for homepage, so this will disable realtime collaboration on homepage
-    if(document.location.pathname > 1) {
+    if(document.location.pathname.length > 1) {
         var documentName = document.location.pathname.substring(1);     // pathname[1] to end
         // we open a sharejs connection to the document, and attach the textArea to the object returned
         // This is for collaborative editing, your textArea will automatically change if someone else makes a change
@@ -33,11 +33,34 @@ var initializeMarkdownEditor = function() {
         return false;
     };
 
+    // Check every second if another user made a change, if he did then update
     setInterval(function() {
         if(didChangeOccur()) {
             convertTextAreaToMarkdown();
         }
     }, 1000);
+
+    // make tab act like tab instead of acting as browser's tab
+    textArea.addEventListener('keydown', function(e) {  // e is the event
+        if(e.keyCode === 9) {   // if tab was pressed
+            // get the cursor position or selection
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
+
+            var target = e.target;
+            var value = target.value;
+
+            // replace selection with a tab at target
+            target.value = value.substring(0, start) + "\t" + value.substring(end);
+
+            // put the cursor back at the right place
+            this.selectionStart = start + 1;
+            this.selectionEnd = start + 1;
+
+            // prevent focus lose (what browser does on tab)
+            e.preventDefault();
+        }
+    });
 
     convertTextAreaToMarkdown();
 };
